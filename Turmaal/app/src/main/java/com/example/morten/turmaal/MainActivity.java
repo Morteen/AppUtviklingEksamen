a28b328671a4f787730ac16a4e426850899ee2dd
morteen <mortenolsen4@gmail.com>
2017-05-29 14:45:30 +0200

    Prøver å legge hvert bilde som bakgrunn i hverliste plassering

--- Turmaal/app/src/main/java/com/example/morten/turmaal/MainActivity.java
+++ Turmaal/app/src/main/java/com/example/morten/turmaal/MainActivity.java
@@ -1,22 +1,66 @@
 package com.example.morten.turmaal;
 
+import android.app.ProgressDialog;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView startListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        //Dette er for å vise bilder fra JsonObjektet
+        // Create global configuration and initialize ImageLoader with this config
+        // Create default options which will be used for every
+//  displayImage(...) call if no options will be passed to this method
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+                .defaultDisplayImageOptions(defaultOptions)
+                .build();
+        ImageLoader.getInstance().init(config);
+        ImageLoader.getInstance().init(config);
+
+
+        //sjekker om brukeren er online
+        if(isOnline()){
+            new JsonStartTask().execute();
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -49,4 +93,139 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    class JsonStartTask extends AsyncTask<String, Void, String> {
+
+        ProgressDialog progressDialog;
+
+String turData=null;
+        @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
+        public JsonStartTask() {
+            super();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("Vent litt nå da..");
+            progressDialog.setTitle("Kobler opp...");
+            progressDialog.show();
+            progressDialog.setProgressStyle(3);
+            progressDialog.setCancelable(false);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+
+            // HttpURLConnection connection= null;
+            BufferedReader reader = null;
+            String line = "";
+            String result = "";
+            InputStream is = null;
+            String turmaalURL="http://itfag.usn.no/~210144/api.php/Turmaal?transform=1";
+
+            HttpURLConnection connection = null;
+            try {
+                URL actorUrl = new URL(turmaalURL);
+
+                connection = (HttpURLConnection) actorUrl.openConnection();
+                connection.connect();
+                int status = connection.getResponseCode();
+                Log.d("connection", "status " + status);
+
+                is = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(is));
+                String responseString;
+                StringBuilder sb = new StringBuilder();
+                while ((responseString = reader.readLine()) != null) {
+                    sb = sb.append(responseString);
+                }
+                turData = sb.toString();
+
+                Log.d("connection", turData);
+
+
+                return turData;
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+
+            } catch (IOException e) {
+                System.out.println("IOException");
+                e.printStackTrace();
+
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            } finally {
+
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                connection.disconnect();
+            }
+
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(String  result) {
+
+            if (result!=null) {
+
+                progressDialog.cancel();
+
+
+                try {
+                    /* DeltarListView = (ListView) view.findViewById(R.id.list_DeltarKurs);
+        KursAdapter adapter = new KursAdapter(getContext(), deltarkursList);*/
+                    startListView=(ListView)findViewById(R.id.startList);
+
+                  TurAdapter adapter= new TurAdapter(getApplicationContext(), Turmaal.lagTurListe(result));
+                    startListView.setAdapter(adapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            } else {
+
+                Toast.makeText(getApplicationContext(), "Fikk ikke hentet noen data", Toast.LENGTH_SHORT).show();
+
+                progressDialog.cancel();
+
+            }
+
+        }
+    }
+
+
+    /**
+     * Sjekker om  det er nettverkstilgang
+     *
+     * @return true or  false
+     */
+    protected boolean isOnline() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(MainActivity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+
+
 }
--- Turmaal/app/src/main/java/com/example/morten/turmaal/TurAdapter.java
+++ Turmaal/app/src/main/java/com/example/morten/turmaal/TurAdapter.java
@@ -1,11 +1,19 @@
 package com.example.morten.turmaal;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 import java.util.ArrayList;
 
@@ -18,6 +26,9 @@ public class TurAdapter extends BaseAdapter {
     Context mContext;
     ArrayList<Turmaal> mineTuraal;
     LayoutInflater mInflater;
+
+
+
 
     public TurAdapter(Context mContext, ArrayList<Turmaal> mineTuraal) {
         this.mContext = mContext;
@@ -43,12 +54,30 @@ public class TurAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView bakgrundsbilde = new ImageView(mContext);
+        //Dette er for å vise bilder fra JsonObjektet
+        // Create global configuration and initialize ImageLoader with this config
+        // Create default options which will be used for every
+//  displayImage(...) call if no options will be passed to this method
+      /*  DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(mContext)
+                .defaultDisplayImageOptions(defaultOptions)
+                .build();
+        ImageLoader.getInstance().init(config);
+        ImageLoader.getInstance().init(config);*/
+
+
+
         ViewHolder viewHolder;
         if (convertView == null) {
 
             //XML fil-navnet
             convertView = mInflater.inflate(R.layout.turmaal_liste, null);
             viewHolder = new ViewHolder();
+            //convertView.setBackgroundResource(R.drawable.test);
 
             ///Dette må tilpasses XML filen
 
@@ -57,6 +86,7 @@ public class TurAdapter extends BaseAdapter {
             viewHolder.tvStartType = (TextView) convertView.findViewById(R.id.tvStartType);
 
             viewHolder.tvStartHoyde = (TextView) convertView.findViewById(R.id.tvStartHoyde);
+            viewHolder.background=(RelativeLayout) convertView.findViewById(R.id.backgrund);
 
 
             convertView.setTag(viewHolder);
@@ -68,7 +98,7 @@ public class TurAdapter extends BaseAdapter {
        Turmaal currentMaal = mineTuraal.get(position);
 
 
-        if (position == 0) {
+        /*if (position == 0) {
             viewHolder.tvStartNavn.setText("Navn\n\n" + currentMaal.getNavn());
             viewHolder.tvStartType.setText("Type\n\n" + currentMaal.getType());
             viewHolder.tvStartHoyde.setText("Høyde\n\n" +Integer.toString(currentMaal.getHoyde()));
@@ -76,9 +106,46 @@ public class TurAdapter extends BaseAdapter {
             viewHolder.tvStartNavn.setText(currentMaal.getNavn());
             viewHolder.tvStartType.setText(currentMaal.getType());
             viewHolder.tvStartHoyde.setText(Integer.toString(currentMaal.getHoyde()));
-        }
+        }*/
+
+        viewHolder.tvStartNavn.setText(currentMaal.getNavn());
+        viewHolder.tvStartType.setText(currentMaal.getType());
+        viewHolder.tvStartHoyde.setText(Integer.toString(currentMaal.getHoyde())+" meter over havet");
+        Log.d("BildeURL",currentMaal.getBilde_URL());
+        //String url="https://jsonparsingdemo-cec5b.firebaseapp.com/jsonData/images/avengers.jpg";//currentMaal.getBilde_URL();
+        String url="https://peakbook.org/gfx/pbes/e1/4f/e14f2c0c9152f1c78681652ff1189f2b/1.jpg";
+
+
+
+        ImageLoader.getInstance().displayImage( url,bakgrundsbilde , new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+
+            }
+
+        });
+
+        convertView.setBackground(bakgrundsbilde.getDrawable());
         return convertView;
     }
+
+
+
 
 
     ///Dette må tilpasses XML filen
@@ -86,6 +153,8 @@ public class TurAdapter extends BaseAdapter {
         public TextView tvStartNavn;
         public TextView tvStartType;
         public TextView tvStartHoyde;
+        public RelativeLayout background;
+
 
     }
 
