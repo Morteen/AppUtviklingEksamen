804ce05188980040c1118c797546a1dabbb3f5b6
morteen <mortenolsen4@gmail.com>
2017-06-03 16:46:37 +0200

    Endring i beskrivelsen

--- Turmaal/app/src/main/java/com/example/morten/turmaal/TurAdapter.java
+++ Turmaal/app/src/main/java/com/example/morten/turmaal/TurAdapter.java
@@ -1,19 +1,13 @@
 package com.example.morten.turmaal;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 
 /**
@@ -52,7 +46,6 @@ public class TurAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView bakgrundsbilde = new ImageView(mContext);
 
 
         ViewHolder viewHolder;
@@ -61,17 +54,16 @@ public class TurAdapter extends BaseAdapter {
             //XML fil-navnet
             convertView = mInflater.inflate(R.layout.turmaal_liste, null);
             viewHolder = new ViewHolder();
-            //convertView.setBackgroundResource(R.drawable.test);
+            convertView.setBackgroundResource(R.drawable.draaper);
 
             ///Dette må tilpasses XML filen
 
             viewHolder.tvStartNavn = (TextView) convertView.findViewById(R.id.tvStartNavn);
-
             viewHolder.tvStartType = (TextView) convertView.findViewById(R.id.tvStartType);
-
             viewHolder.tvStartHoyde = (TextView) convertView.findViewById(R.id.tvStartHoyde);
-            viewHolder.background = (RelativeLayout) convertView.findViewById(R.id.backgrund);
-            //viewHolder.visURl=(TextView)convertView.findViewById(R.id.url) ;
+            viewHolder.tvAvstand=(TextView)convertView.findViewById(R.id.tAvstand);
+
+
 
             convertView.setTag(viewHolder);
         } else {
@@ -85,13 +77,14 @@ public class TurAdapter extends BaseAdapter {
         viewHolder.tvStartNavn.setText(currentMaal.getNavn());
         viewHolder.tvStartType.setText(currentMaal.getType());
         viewHolder.tvStartHoyde.setText(Integer.toString(currentMaal.getHoyde()) + " meter over havet");
+        viewHolder.tvAvstand.setText(Integer.toString(currentMaal.getHoyde()));
 
         Log.d("BildeURL", currentMaal.getBilde_URL());
 
 
-        new DownloadImageTask(bakgrundsbilde).execute("http://static.panoramio.com/photos/large/91778790.jpg");
 
-        convertView.setBackground(bakgrundsbilde.getDrawable());
+
+
         return convertView;
     }
 
@@ -101,41 +94,10 @@ public class TurAdapter extends BaseAdapter {
         public TextView tvStartNavn;
         public TextView tvStartType;
         public TextView tvStartHoyde;
-        public TextView visURl;
-        public RelativeLayout background;
-
-
-    }
-
-    //Fra læreboken
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            Bitmap mIcon11 = null;
-            try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return mIcon11;
-        }
-
-        protected void onPostExecute(Bitmap result) {
+       public TextView tvAvstand;
 
 
 
-
-
-            bmImage.setImageBitmap(result);
-        }
     }
 
 
